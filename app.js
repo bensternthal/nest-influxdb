9bd8ba8561e87dd914efdd626735690541aeef45
Benjamin Sternthal <ben@devpatch.com>
2016-08-30 13:52:00 -0700

    Dockerized & added slackbot.

--- app.js
+++ app.js
@@ -3,6 +3,17 @@
 var conf = require('./lib/conf');
 var Nest = require('./lib/nestClient');
 var Influx = require('./lib/influx');
+var SlackBot = require('slackbots');
+
+// Create & Configure Slackbot
+var bot = new SlackBot({
+    token: conf.get('slackbot:api_token'),
+    name: 'nest-status'
+});
+var channel = conf.get('slackbot:channel');
+var params = {icon_emoji: ':nest:'};
+
+bot.postMessageToGroup(channel, 'Nest Has Started', params);
 
 function getData() {
     Nest.getNestData().then(function(nestData) {
@@ -11,9 +22,9 @@ function getData() {
         setTimeout(getData, 300000);
 
     }, function(error) {
+        bot.postMessageToGroup(channel, 'Nest Error: ' + error, params);
         console.error('Failed!', error);
     });
 };
-
 
 getData();
